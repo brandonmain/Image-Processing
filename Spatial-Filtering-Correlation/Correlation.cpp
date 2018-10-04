0eaf5f1c1def295a493a02e636ae2620bea76498
Brandon Main <brandon@Brandons-MacBook-Pro.local>
2018-10-03 19:59:10 -0700

    New project files

---
+++ Spatial-Filtering-Correlation/Correlation.cpp
@@ -0,0 +1,29 @@
+/**
+ * Spatial Filtering (Correlation)
+ * 
+ * 
+ * 	by: Brandon Main
+ * 	
+ * 	compile with: g++ $(pkg-config --cflags --libs opencv) Correlation.cpp -o Correlation 
+ */
+
+#include "imageLib.h"
+
+int main(int argc, char** argv)
+{
+
+	//Read image in from command line.
+	Mat original_image = imread(argv[1], IMREAD_GRAYSCALE);
+
+	//Write image back to file just in case it's not
+	//in the proper format for processing.
+	writeIMG(argv[1], original_image);
+
+
+	/////////////////////////////////////////////////////
+	// Need to write a function to perform correlation //
+	/////////////////////////////////////////////////////
+
+
+	return 0;
+}
