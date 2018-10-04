5c4d0c15ec376f727ef9eb04762c97877f091e9b
Brandon Main <brandon@Brandons-MacBook-Pro.local>
2018-10-03 21:30:51 -0700

    Added better formatting

--- Spatial-Filtering-Correlation/imageLib.h
+++ Spatial-Filtering-Correlation/imageLib.h
@@ -17,7 +17,7 @@ using namespace std;
  */
 void displayIMG(const char* WINDOW, Mat &image)
 {
-	namedWindow( WINDOW, WINDOW_AUTOSIZE );// Create a WINDOW for display.
+    namedWindow( WINDOW, WINDOW_AUTOSIZE );// Create a WINDOW for display.
     imshow( WINDOW, image );    // Show our image inside it.
 
     waitKey(0);      // Wait for a keystroke in the WINDOW
@@ -25,16 +25,16 @@ void displayIMG(const char* WINDOW, Mat &image)
 
 /**
  * @brief      Writes an image to a file in binary (0) format
- *			   for pgm images.
+ *             for pgm images.
  *
  * @param[in]  FILE   The file
  * @param      image  The image
  */
 void writeIMG(const char* FILE, Mat &image)
 {
-	vector<int> compression_params;
+    vector<int> compression_params;
     compression_params.push_back(CV_IMWRITE_PXM_BINARY);
     compression_params.push_back(0);
 
-	imwrite(FILE, image, compression_params);
+    imwrite(FILE, image, compression_params);
 }
