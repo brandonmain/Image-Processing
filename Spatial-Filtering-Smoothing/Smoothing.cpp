7bc065bb5b0488875c4e2a9512b3f708d0c8cd9d
Brandon Main <brandon@Brandons-MacBook-Pro.local>
2018-10-07 13:44:08 -0700

    added

---
+++ Spatial-Filtering-Smoothing/Smoothing.cpp
@@ -0,0 +1,158 @@
+/**
+ * Spatial Filtering (Smoothing)
+ * 
+ * @brief This program will smooth an image
+ *        
+ * Usage:  ~$ ./Smoothing image.pgm
+ *  
+ */  
+
+#include "imageLib.h"
+
+Mat addPadding(Mat &image, const int SIZE);
+Mat crop(Mat &image, const int SIZE);
+void avg7x7(Mat &image);
+void avg15x15(Mat &image);
+
+
+
+int main(int argc, char** argv)
+{
+
+	Mat image = imread(argv[1], IMREAD_GRAYSCALE);
+
+	int choice = 0;
+
+	cout << endl << "Enter your choice:" << endl;
+	cout << "1. Smooth using 7x7 averaging filter." << endl;
+	cout << "2. Smooth using 15x15 averaging filter." << endl;
+	cout << "3. Smooth using 7x7 gaussian filter." << endl;
+	cout << "4. Smooth using 15x15 gaussian filter." << endl;
+	cin >> choice;
+
+	switch(choice)
+	{
+		case 1: //7x7 avg
+		{
+			avg7x7(image);
+			break;
+		}
+		case 2: //15x15 avg
+		{
+			avg15x15(image);
+			break;
+		}
+		case 3: //7x7 gaussian
+		{
+			break;
+		}
+		case 4: //15x15 gaussian
+		{
+			break;
+		}
+	}
+	return 0;
+}
+
+
+Mat addPadding(Mat &image, const int SIZE)
+{
+	Mat new_image(image.rows + 2*(SIZE - 1), image.cols + 2*(SIZE - 1), CV_8U);
+
+	//Add 0 padding
+	for (int i = 0; i < new_image.rows; ++i)
+	{
+		for (int j = 0; j < new_image.cols; ++j)
+		{
+			new_image.at<unsigned char>(i,j) = 0;
+		}
+	}
+
+	for (int i = SIZE - 1; i < new_image.rows - SIZE - 1; ++i)
+	{
+		for (int j = SIZE - 1; j < new_image.rows - SIZE - 1; ++j)
+		{
+			new_image.at<unsigned char>(i,j) = image.at<unsigned char>(i - (SIZE - 1), j - (SIZE - 1));
+		}	
+	}
+
+	return new_image;
+}
+
+Mat crop(Mat &image, const int SIZE)
+{
+	Mat new_image(image.rows - 2*(SIZE+1), image.cols - 2*(SIZE+1), CV_8U);
+
+	for (int i = 0; i < new_image.rows; ++i)
+	{
+		for (int j = 0; j < new_image.cols; ++j)
+		{
+			new_image.at<unsigned char>(i,j) = image.at<unsigned char>(i + SIZE - 1, j + SIZE - 1);
+		}
+	}
+
+	return new_image;
+}
+
+void avg7x7(Mat &image)
+{
+	Mat padded_image(addPadding(image, 7));
+	Mat averaged_image(padded_image.rows, padded_image.cols, CV_8U);
+	int sum = 0;
+
+	//Loop over entire image
+    for (int pad_rows = 0; pad_rows < padded_image.rows; ++pad_rows)
+    {
+        for (int pad_cols = 0; pad_cols < padded_image.cols; ++pad_cols)
+        {
+            //Loop over mask
+            for (int i = 0; i < 7; ++i)
+            {
+                for (int j = 0; j < 7; ++j)
+                {
+                    sum += image.at<unsigned char>(pad_rows + i, pad_cols + j);
+                }
+            }
+            averaged_image.at<unsigned char>(pad_rows,pad_cols) = sum/49;
+            sum = 0;
+        }
+    }
+
+    Mat cropped_image(crop(averaged_image, 7));
+    displayIMG("original", image);
+    displayIMG("Smoothed 7x7 avg", cropped_image);
+
+
+}
+
+void avg15x15(Mat &image)
+{
+	Mat padded_image(addPadding(image, 15));
+	Mat averaged_image(padded_image.rows, padded_image.cols, CV_8U);
+	int sum = 0;
+
+	//Loop over entire image
+    for (int pad_rows = 0; pad_rows < padded_image.rows; ++pad_rows)
+    {
+        for (int pad_cols = 0; pad_cols < padded_image.cols; ++pad_cols)
+        {
+            //Loop over mask
+            for (int i = 0; i < 15; ++i)
+            {
+                for (int j = 0; j < 15; ++j)
+                {
+                    sum += image.at<unsigned char>(pad_rows + i, pad_cols + j);
+                }
+            }
+            averaged_image.at<unsigned char>(pad_rows,pad_cols) = sum/225;
+            sum = 0;
+        }
+    }
+
+    Mat cropped_image(crop(averaged_image, 15));
+    displayIMG("original", image);
+    displayIMG("Smoothed 15x15 avg", cropped_image);
+
+}
+
+
